95c5ab1b8dfa6d6d1368c888e3d67363c596b9ee
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-15 22:47:10 +0200

    crate user info model

--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -1,31 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/models/user_info_model.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
 
 class UserInfoListTile extends StatelessWidget {
-  const UserInfoListTile({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subTitle,
-  });
+  const UserInfoListTile({super.key, required this.userInfoModel});
 
-  final String image, title, subTitle;
+  final UserInfoModel userInfoModel;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 0,
       color: Color(0xffFAFAFA),
-      child: ListTile(
-        leading: SvgPicture.asset(image),
-        title: Text(
-          title,
-          style: Styles.styleSemiBold16,
-        ),
-        subtitle: Text(
-          subTitle,
-          style: Styles.styleRegular12,
+      child: Center(
+        child: ListTile(
+          leading: SvgPicture.asset(userInfoModel.image),
+          title: Text(
+            userInfoModel.title,
+            style: Styles.styleSemiBold16,
+          ),
+          subtitle: Text(
+            userInfoModel.subTitle,
+            style: Styles.styleRegular12,
+          ),
         ),
       ),
     );
