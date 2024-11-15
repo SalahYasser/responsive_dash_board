8516b5ee8a00cec893c252ba3ee721c5ca29801a
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-15 22:48:28 +0200

    crate latest transaction list view

--- lib/widgets/quick_in_voice.dart
+++ lib/widgets/quick_in_voice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/widgets/latest_transaction_list_view.dart';
 import 'package:responsive_dash_board/widgets/quick_in_voice_header.dart';
 
 class QuickInVoice extends StatelessWidget {
@@ -8,7 +9,12 @@ class QuickInVoice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomBackgroundContainer(
-      child: QuickInVoiceHeader(),
+      child: Column(
+        children: [
+          QuickInVoiceHeader(),
+          LatestTransactionListView(),
+        ],
+      ),
     );
   }
 }
