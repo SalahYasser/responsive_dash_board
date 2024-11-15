6edc48dce06bf5b8a2c64cd85e1915f5e2e53318
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-15 03:49:53 +0200

    create drawer item list view

---
+++ lib/widgets/drawer_item_list_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/custom_item.dart';
+
+class DrawerItemListView extends StatelessWidget {
+  const DrawerItemListView({super.key});
+
+  static const List<DrawerItemModel> items = [
+    DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
+    DrawerItemModel(title: 'My Transaction', image: Assets.imagesMyTransctions),
+    DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
+    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWalletAccount),
+    DrawerItemModel(title: 'My Investments', image: Assets.imagesMyInvestments),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: CustomItem(
+            drawerItemModel: items[index],
+          ),
+        );
+      },
+    );
+  }
+}
