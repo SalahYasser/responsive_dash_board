063bd5af3b5b4e72c09b21261f7294c02deb3fe9
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-15 16:12:11 +0200

    create all expenses

---
+++ lib/widgets/all_expenses.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AllExpenses extends StatelessWidget {
+  const AllExpenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+
+      ),
+    );
+  }
+}
+
