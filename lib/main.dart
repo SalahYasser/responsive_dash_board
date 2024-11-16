bb69c47a28fa190519455b32d69555c9b2c73348
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-17 01:29:28 +0200

    app bar to dashboard mobile layout done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/views/dash_board_view.dart';
+import 'package:responsive_dash_board/views/dashboard_view.dart';
 
 void main() {
   runApp(const ResponsiveDashBoard());
---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
+
+class DashBoardView extends StatefulWidget {
+  const DashBoardView({super.key});
+
+  @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              elevation: 0,
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
+                icon: Icon(Icons.menu),
+              ),
+              backgroundColor: Color(0xffFAFAFA),
+            )
+          : null,
+      drawer: MediaQuery.sizeOf(context).width < 800 ? CustomDrawer() : null,
+      backgroundColor: Color(0xffEFEFEF),
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const DashBoardMobileLayout(),
+        tabletLayout: (context) => const DashBoardTabletLayout(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
+  }
+}
