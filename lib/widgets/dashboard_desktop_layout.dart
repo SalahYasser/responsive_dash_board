651f68fab72c11065e7e0a5e4fdd1e65410dc849
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-16 18:47:08 +0200

    create transaction history header

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_transaction_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/my_cards_section.dart';
@@ -19,11 +20,41 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 24),
         Expanded(
           flex: 2,
-          child: MyCardsSection(),
-        )
+          child: Column(
+            children: [
+              Expanded(
+                child: MyCardsSection(),
+              ),
+              Expanded(
+                child: TransactionHistoryHeader(),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
 }
 
+class TransactionHistoryHeader extends StatelessWidget {
+  const TransactionHistoryHeader({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Transaction History',
+          style: Styles.styleSemiBold20,
+        ),
+        Text(
+          'See all',
+          style: Styles.styleMedium16.copyWith(
+            color: Color(0xff4EB7F2),
+          ),
+        ),
+      ],
+    );
+  }
+}
