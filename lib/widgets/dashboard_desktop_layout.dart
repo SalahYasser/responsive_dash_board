7d763610f20b1103762366a42a4bdc0740b79140
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-16 19:30:51 +0200

    simple refactor

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_transaction_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/my_cards_section.dart';
-import 'package:responsive_dash_board/widgets/transaction_history.dart';
+import 'package:responsive_dash_board/widgets/my_cards_and_transaction_history_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -20,20 +19,10 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 24),
         Expanded(
           flex: 2,
-          child: Column(
-            children: [
-              Expanded(
-                child: MyCardsSection(),
-              ),
-              Expanded(
-                child: TransactionHistory(),
-              ),
-            ],
-          ),
+          child: MyCardsAndTransactionHistorySection(),
         ),
       ],
     );
   }
 }
-
 
