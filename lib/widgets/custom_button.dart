78c4c572523cd1caf5fb33beb585424f841838e7
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-16 02:10:30 +0200

    create custom button

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key, this.bgColor, required this.text, this.textColor});
+
+  final Color? bgColor, textColor;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 62,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: bgColor ?? Color(0xff4EB7F2),
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: Styles.styleSemiBold18.copyWith(
+            color: textColor ?? Color(0xffFFFFFF),
+          ),
+        ),
+      ),
+    );
+  }
+}
